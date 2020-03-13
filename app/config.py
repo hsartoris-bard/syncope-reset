b48208c48470355731fff5c90e89fbef72128dd6
Hayden Sartoris <hsartoris@bard.edu>
2020-03-13 14:33:53 -0400

    couple of small patches for 'prod'

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
-#SERVER_NAME = 'cas02.bard.edu'
-SERVER_NAME = 'localhost:5000'
-PREFIX = 'ad_reset'
+SERVER_NAME = 'cas02.bard.edu'
+#SERVER_NAME = 'localhost:5000'
+PREFIX = 'ad-reset'
 FLASK_DEBUG = 'true'
